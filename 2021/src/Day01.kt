cb6f6444df7ebcc6cffed44eb15546e8fecc7db3
Marcel Scheck <marcel.scheck@hetzner-cloud.de>
2021-12-04 00:04:32 +0100

    Resolve Day1

--- 2021/src/Day01.kt
+++ 2021/src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.windowed(2).count{it[0] < it[1]}
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(4).count{it[0] < it[3]}
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
+
+    println(part1(testInput))
+    check(part1(testInput) == 7)
     println(part1(input))
+
+    println(part2(testInput))
+    check(part1(testInput) == 5)
     println(part2(input))
 }
